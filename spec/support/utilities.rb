e029ad95c44c209e12de6d13af08d705510acfe0
Barak Harizi <barak.h@fiverr.com>
2016-04-26 15:32:11 +0300

    Chapter 8 exercises

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -11,3 +11,15 @@ RSpec::Matchers.define :have_error_message do |message|
     page.should have_selector('div.alert.alert-error', text: message)
   end
 end
+
+RSpec::Matchers.define :have_title do |text|
+  match do |page|
+    page.should have_selector('title', text: text)
+  end
+end
+
+RSpec::Matchers.define :have_h1 do |text|
+  match do |page|
+    page.should have_selector('h1', text: text)
+  end
+end
